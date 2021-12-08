0eb9e86627f1723873b89943d282395d05836e36
isaiasvillalobos <isaiasvillalobos98@gmail.com>
2021-12-08 18:20:22 -0500

    The CSS for the Login.css and Login.js component has been adjusted.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./styles/Login.css";
-import logo from "./images/logo192.png";
+import logo from "./images/img_avatar.png";
+
+
 const Login = () => {
+  const [RememberMe, setRememberMe] = useState(false);
+  const handleOnCheckChange = () => {
+    setRememberMe(!RememberMe)
+  }
+
+
   return (
     <form>
       <div className="imgcontainer">
         <img src={logo} alt="Avatar" className="avatar" />
       </div>
 
-      <label for="uname">
+      <label>
         <b>Username</b>
-        <input type="text" placeholder="Enter Username" name="uname" required />
+        <input type="text" placeholder="Enter Username" name="uname" className="login-user" required />
       </label>
 
-      <label for="psw">
+      <label>
         <b>Password</b>
         <input
           type="password"
           placeholder="Enter Password"
           name="psw"
+          className="login-psw"
           required
         />
       </label>
 
-      <button type="submit">Login</button>
       <label>
-        <input type="checkbox" checked="checked" name="remember" /> Remember me
+        <input type="checkbox" checked={RememberMe} onChange={handleOnCheckChange} name="remember" /> Remember me
       </label>
 
       <div className="container">
-        <button type="button" className="cancelbtn">
-          Cancel
+        <button type="submit" className="loginbtn">
+          Login
         </button>
-          <button type="button" className="forgotPsw">Forget Password ?</button>
+          <button type="button" className="forgotPsw">Forget Password</button>
       </div>
     </form>
   );
